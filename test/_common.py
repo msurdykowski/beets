5aac82ed819139e15b085422523b2aa7bfa1c8b0
msurdykowski <msurdykowski@gmail.com>
2016-09-06 14:15:04 +0200

    Test
    taki sobie test

--- test/_common.py
+++ test/_common.py
@@ -13,7 +13,7 @@
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
 
-"""Some common functionality for beets' test cases."""
+"""Some common functionality for beets' test cases.  ssss"""
 from __future__ import division, absolute_import, print_function
 
 import time
